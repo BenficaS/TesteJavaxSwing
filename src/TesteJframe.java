53aa1db0e980a3c3eea82c6664ef88b201842c3e
12111280 <Biell.Slip@gmail.com>
2023-03-14 20:21:45 -0300

    Feat: cria optionpane.

---
+++ src/TesteJframe.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+public class TesteJframe {
+    public static void main(String[] args) {
+        JFrame janela=new JFrame("Meu primeiro frame em java");
+        janela.setSize(400,400);
+        janela.setVisible(true);
+    }
+}
